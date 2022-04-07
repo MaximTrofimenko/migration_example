682aca1ea92fb720fe4e699b540fd7069a70bc3d
Maxim Trofimenko <maxim.trofimenko@corp.codeinside.ru>
2022-04-07 08:45:03 +0300

    Initial commit

---
+++ src/main/java/com/trofimenko/migration_example/MigrationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.trofimenko.migration_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationExampleApplication.class, args);
+	}
+
+}
